c64cfc884b7784c535d497758e59cc84045774de
sameehkhan <sameehkhan1@gmail.com>
2018-11-29 14:55:53 -0800

    Add html jquery

--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -4,11 +4,23 @@ class DomNodeCollection{
     
   }
 
+  html(html) {
+   if (typeof html === "string") {
+     this.each((node) => {
+       node.innerHTML = html;
+     });
+   } else if (this.nodes.length > 0) {
+     return this.nodes[0].innerHTML;
+   }
+ }
+
+ empty() {
+   this.html('');
+ }
+ 
 }
 
-DomNodeCollection.prototype.html = function(){
-  
-};
+
 DomNodeCollection.prototype.empty = function(){
   
 };
